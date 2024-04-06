8ef9df80004c6ef85f120745c99e7bb35a11d663
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-06 19:36:03 -0300

    TEST: added test 'equalsName' in class 'PlayableTest'

--- src/test/scala/PlayableTest.scala
+++ src/test/scala/PlayableTest.scala
@@ -2,6 +2,19 @@ import munit.FunSuite
 
 class PlayableTest extends FunSuite{
   var Cristiano: Character = _
+  var Messi: Character = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    var Cristiano = Character()
+    var Messi = Character()
+  }
+
+  test("equalsname"){
+    val nombre = Cristiano.sayName("Cristiano")
+    val expected = "Cristiano"
+    assertEquals(nombre,expected)
+
+  }
 
 
 }
