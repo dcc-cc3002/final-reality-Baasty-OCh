6ff4bb4d134e16098b3f536befaec7c98d653786
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-06 19:19:46 -0300

    DOC: added methods 'sayName','sayLife','sayDefence','sayWeight','sayKind','checkWeapon' in trait Playable

--- src/main/scala/model/playable/Playable.scala
+++ src/main/scala/model/playable/Playable.scala
@@ -1,5 +1,13 @@
 package model.playable
 
 trait Playable {
+  def sayname(name: String) : Unit
+  def saylife(life: Int): Unit
+  def saydefence(defence: Int): Unit
+  def sayweight(weight: Int): Unit
+  def saykind(kind: String): Unit
+  def checkWeapon(player:Playable): Unit
+
+
 
 }
