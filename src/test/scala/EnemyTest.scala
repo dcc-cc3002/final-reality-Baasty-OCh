4a7cb9595c4703028c1b418107bcd506cd1bf5b4
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-07 15:58:14 -0400

    FEAT: added test 'equalsName','NotEqualsName', 'equalsWeight','NotEqualsWeight','equalsAttack','NotEqualsAttack', 'equalOwner', 'NotEqualsOwner' in class 'WeaponTest'

--- src/test/scala/EnemyTest.scala
+++ src/test/scala/EnemyTest.scala
@@ -2,72 +2,20 @@ import munit.FunSuite
 import model.nonplayable.{MagicWeapon, NonPlayable, Weapon,Enemy}
 import model.playable.{Character, MagicCharacter, Playable}
 class EnemyTest extends FunSuite {
-  var Sword: Weapon = _
-  var Bow: Weapon = _
   var Wand: MagicWeapon = _
   var Staff: MagicWeapon = _
-  var Cristiano: Character = _
-  var Messi: Character = _
   var Alexis: MagicCharacter = _
   var Vidal: MagicCharacter = _
   var Neymar: Enemy = _
   var Bale: Enemy = _
   override def beforeEach(context: BeforeEach): Unit = {
-    Sword = new Weapon()
-    Bow = new Weapon()
     Wand = new MagicWeapon()
     Staff = new MagicWeapon()
-    Cristiano = new Character()
-    Messi = new Character()
     Alexis = new MagicCharacter()
     Vidal = new MagicCharacter()
     Neymar = new Enemy()
     Bale = new Enemy()
 
-  }
-
-  test("equalsName") {
-    var nombre: String = Sword.sayName("Espada")
-    var expected = "Espada"
-    assertEquals(nombre, expected)
-  }
-  test("NotEqualsName") {
-    var nombre1 = Sword.sayName("Espada")
-    var nombre2 = Bow.sayName("Arco")
-    assertNotEquals(nombre1, nombre2)
-  }
-  test("equalsWeight") {
-    var peso = Sword.sayWeight(5)
-    var expected: Int = 5
-    assertEquals(peso, expected)
-  }
-  test("NotEqualsWeight") {
-    var peso1 = Sword.sayWeight(5)
-    var peso2 = Bow.sayWeight(3)
-    assertNotEquals(peso1, peso2)
-  }
-  test("equalsAttack") {
-    var ataque = Sword.sayAttack(25)
-    var expected: Int = 25
-    assertEquals(ataque, expected)
-
-  }
-  test("NotEqualsAttack") {
-    var vida1 = Sword.sayAttack(25)
-    var vida2 = Bow.sayAttack(20)
-    assertNotEquals(vida1, vida2)
-
-  }
-  test("equalsOwner") {
-    var dueño: Character = Sword.sayOwner(Cristiano)
-    var expected : Character = Cristiano
-    assertEquals(dueño, expected)
-  }
-
-  test("NotEqualsOwner") {
-    var dueño1: Character = Sword.sayOwner(Cristiano)
-    var dueño2: Character = Bow.sayOwner(Messi)
-    assertNotEquals(dueño1, dueño2)
   }
 
   test("equalsMagicOwner") {
--- src/test/scala/WeaponTest.scala
+++ src/test/scala/WeaponTest.scala
@@ -1,3 +1,60 @@
-class WeaponTest {
+import munit.FunSuite
+import model.nonplayable.{MagicWeapon, NonPlayable, Weapon,Enemy}
+import model.playable.{Character, MagicCharacter, Playable}
+class WeaponTest extends FunSuite {
+  var Sword: Weapon = _
+  var Bow: Weapon = _
+  var Cristiano: Character = _
+  var Messi: Character = _
 
+  override def beforeEach(context: BeforeEach): Unit = {
+    Sword = new Weapon()
+    Bow = new Weapon()
+    Cristiano = new Character()
+    Messi = new Character()
+  }
+
+  test("equalsName") {
+    var nombre: String = Sword.sayName("Espada")
+    var expected = "Espada"
+    assertEquals(nombre, expected)
+  }
+  test("NotEqualsName") {
+    var nombre1 = Sword.sayName("Espada")
+    var nombre2 = Bow.sayName("Arco")
+    assertNotEquals(nombre1, nombre2)
+  }
+  test("equalsWeight") {
+    var peso = Sword.sayWeight(5)
+    var expected: Int = 5
+    assertEquals(peso, expected)
+  }
+  test("NotEqualsWeight") {
+    var peso1 = Sword.sayWeight(5)
+    var peso2 = Bow.sayWeight(3)
+    assertNotEquals(peso1, peso2)
+  }
+  test("equalsAttack") {
+    var ataque = Sword.sayAttack(25)
+    var expected: Int = 25
+    assertEquals(ataque, expected)
+
+  }
+  test("NotEqualsAttack") {
+    var vida1 = Sword.sayAttack(25)
+    var vida2 = Bow.sayAttack(20)
+    assertNotEquals(vida1, vida2)
+
+  }
+  test("equalsOwner") {
+    var dueño: Character = Sword.sayOwner(Cristiano)
+    var expected: Character = Cristiano
+    assertEquals(dueño, expected)
+  }
+
+  test("NotEqualsOwner") {
+    var dueño1: Character = Sword.sayOwner(Cristiano)
+    var dueño2: Character = Bow.sayOwner(Messi)
+    assertNotEquals(dueño1, dueño2)
+  }
 }
