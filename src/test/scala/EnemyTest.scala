f6b076277cf7552350d9f66616c02a177deeaadc
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-07 16:09:11 -0400

    FEAT: added test 'equalsName','NotEqualsName', 'equalsWeight','NotEqualsWeight','equalsAttack','NotEqualsAttack','equalsAttack', 'NotEqualsAttack','equalsDefence','NotEqualsdefence' in class 'EnemyTest'

--- src/test/scala/EnemyTest.scala
+++ src/test/scala/EnemyTest.scala
@@ -20,5 +20,49 @@ class EnemyTest extends FunSuite {
     var nombre2: String = Bale.sayName("Bale")
     assertNotEquals(nombre1,nombre2)
   }
+  test("equalsWeight") {
+    var peso = Neymar.sayWeight(5)
+    var expected: Int = 5
+    assertEquals(peso, expected)
+  }
+  test("NotEqualsWeight") {
+    var peso1 = Neymar.sayWeight(5)
+    var peso2 = Bale.sayWeight(3)
+    assertNotEquals(peso1, peso2)
+  }
+  test("equalsAttack") {
+    var ataque = Neymar.sayAttack(25)
+    var expected: Int = 25
+    assertEquals(ataque, expected)
+
+  }
+  test("NotEqualsAttack") {
+    var vida1 = Neymar.sayAttack(25)
+    var vida2 = Bale.sayAttack(20)
+    assertNotEquals(vida1, vida2)
+
+  }
+  test("equalsLife") {
+    var vida = Neymar.sayLife(100)
+    var expected: Int = 100
+    assertEquals(vida, expected)
+
+  }
+  test("NotEqualsLife") {
+    var vida1 = Neymar.sayLife(100)
+    var vida2 = Bale.sayLife(90)
+    assertNotEquals(vida1, vida2)
+
+  }
+  test("equalsDefence") {
+    var defensa = Neymar.sayDefence(10)
+    var expected: Int = 10
+    assertEquals(defensa, expected)
+  }
+  test("NotEqualsDefence") {
+    var defensa1 = Neymar.sayDefence(10)
+    var defensa2 = Bale.sayDefence(5)
+    assertNotEquals(defensa1, defensa2)
+  }
 }
 
