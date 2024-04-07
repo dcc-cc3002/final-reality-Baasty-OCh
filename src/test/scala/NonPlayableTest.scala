07aafc6aa81d9dfe26908f196e75b068d237c5f2
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-07 13:21:48 -0400

    FEAT: added  class 'NonPlayableTest' in package 'test'

--- src/test/scala/NonPlayableTest.scala
+++ src/test/scala/NonPlayableTest.scala
@@ -3,12 +3,9 @@ import model.nonplayable.{Weapon,NonPlayable}
 class NonPlayableTest extends FunSuite {
   var Sword: NonPlayable = _
   var Bow: NonPlayable = _
-  var Alexis: MagicCharacter = _
-  var Vidal: MagicCharacter = _
+
   override def beforeEach(context: BeforeEach): Unit = {
-    Cristiano = new Character()
-    Messi = new Character()
-    Alexis = new MagicCharacter()
-    Vidal = new MagicCharacter()
+    Sword = new Weapon()
+    Bow = new Weapon()
 
   }
