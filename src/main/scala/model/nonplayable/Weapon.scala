7dbfe9351ad50fbbf1a3a48b9083624eda4c663c
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-07 13:12:59 -0400

    FEAT: added  method 'sayWeight' in class 'Weapon' in package 'nonplayable'

--- src/main/scala/model/nonplayable/Weapon.scala
+++ src/main/scala/model/nonplayable/Weapon.scala
@@ -1,9 +1,13 @@
 package model.nonplayable
 import model.nonplayable.Nonplayable
-class Weapon  {
+class Weapon extends Nonplayable {
   def sayName(name: String) : String = {
     return name
   }
+  def sayWeight(weight: Int): Int = {
+    return weight
+  }
+
 
 
 }
