9eecf39a3aa1fabb0b987443620c1d0de4f03dce
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-07 15:37:05 -0400

    FEAT: added  methods 'sayLife','sayDefence','sayName','sayWeight','sayAttack' in class 'Enemy' in package 'nonplayable'

--- src/main/scala/model/nonplayable/Enemy.scala
+++ src/main/scala/model/nonplayable/Enemy.scala
@@ -1,5 +1,24 @@
 package model.nonplayable
-import model.nonplayable
-class Enemy {
+import model.nonplayable.NonPlayable
+class Enemy extends NonPlayable{
 
+  def sayName(name: String): String = {
+    return name
+  }
+
+  def sayWeight(weight: Int): Int = {
+    return weight
+  }
+
+  def sayAttack(attack: Int): Int = {
+    return attack
+  }
+
+  def sayLife(life: Int): Int = {
+    return life
+  }
+
+  def sayDefence(defence: Int): Int = {
+    return defence
+  }
 }
