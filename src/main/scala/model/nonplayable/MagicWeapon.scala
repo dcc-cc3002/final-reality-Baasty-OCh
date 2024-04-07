89be0f2ed899aef531a6f871edf68f30f1ce3a6f
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-07 14:30:19 -0400

    FEAT: override  method 'sayOwner' in class 'MagicWeapon' in package 'nonplayable'

--- src/main/scala/model/nonplayable/MagicWeapon.scala
+++ src/main/scala/model/nonplayable/MagicWeapon.scala
@@ -1,9 +1,13 @@
 package model.nonplayable
 import model.nonplayable.Weapon
+import model.playable.MagicCharacter
 
 class MagicWeapon extends Weapon {
   def sayMana(mana:Int): Int = {
     return mana
+  }
+  override def sayOwner(magicOwner : MagicCharacter): MagicCharacter = {
+    return magicOwner
   }
 
 }
