3dbd21fce2644c9e0a8d1abed5d39a69362975d3
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-07 08:52:09 -0400

    DOC: added methods 'sayName','sayLife','sayDefence','sayWeight','sayKind','putWeapon' in class 'MagicCharacter'

--- src/main/scala/model/playable/MagicCharacter.scala
+++ src/main/scala/model/playable/MagicCharacter.scala
@@ -1,5 +1,30 @@
 package model.playable
 
 class MagicCharacter {
+  def sayName(name: String): String = {
+    return name
+
+  }
+
+  def sayLife(life: Int): Int = {
+    return life
+  }
+
+  def sayDefence(defence: Int): Int = {
+    return defence
+  }
+
+  def sayWeight(weight: Int): Int = {
+    return weight
+  }
+
+  def sayKind(kind: String): String = {
+    return kind
+  }
+
+  def putWeapon(weapon: String): String = {
+    return weapon
+
+  }
 
 }
