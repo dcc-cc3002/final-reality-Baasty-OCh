d0f854aca486d8a292fb1145bb8c552ae3ecd65c
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-12 14:17:56 -0400

    REFACTOR: edited test 'equalsName','NotequalsName',,'equalsOwner','NotEqualsOwner','equalsWeight','NotEqualsWeight','equalsAttack','NotEqualsAttack' in Test 'WeaponTest'

--- src/test/scala/WeaponTest.scala
+++ src/test/scala/WeaponTest.scala
@@ -8,53 +8,53 @@ class WeaponTest extends FunSuite {
   var Messi: Character = _
 
   override def beforeEach(context: BeforeEach): Unit = {
-    Sword = new Weapon()
-    Bow = new Weapon()
-    Cristiano = new Character()
-    Messi = new Character()
+    Sword = new Weapon("Espada",70,100,Cristiano)
+    Bow = new Weapon("Arco",25,50,Messi)
+    //Cristiano = new Character()
+    //Messi = new Character()
   }
 
   test("equalsName") {
-    var nombre: String = Sword.sayName("Espada")
+    var nombre: String = Sword.sayName()
     var expected = "Espada"
     assertEquals(nombre, expected)
   }
   test("NotEqualsName") {
-    var nombre1 = Sword.sayName("Espada")
-    var nombre2 = Bow.sayName("Arco")
+    var nombre1 = Sword.sayName()
+    var nombre2 = Bow.sayName()
     assertNotEquals(nombre1, nombre2)
   }
   test("equalsWeight") {
-    var peso = Sword.sayWeight(5)
-    var expected: Int = 5
+    var peso = Sword.sayWeight()
+    var expected: Int = 70
     assertEquals(peso, expected)
   }
   test("NotEqualsWeight") {
-    var peso1 = Sword.sayWeight(5)
-    var peso2 = Bow.sayWeight(3)
+    var peso1 = Sword.sayWeight()
+    var peso2 = Bow.sayWeight()
     assertNotEquals(peso1, peso2)
   }
   test("equalsAttack") {
-    var ataque = Sword.sayAttack(25)
-    var expected: Int = 25
+    var ataque = Sword.sayAttack()
+    var expected: Int = 100
     assertEquals(ataque, expected)
 
   }
   test("NotEqualsAttack") {
-    var vida1 = Sword.sayAttack(25)
-    var vida2 = Bow.sayAttack(20)
+    var vida1 = Sword.sayAttack()
+    var vida2 = Bow.sayAttack()
     assertNotEquals(vida1, vida2)
 
   }
   test("equalsOwner") {
-    var dueño: Character = Sword.sayOwner(Cristiano)
+    var dueño: Character = Sword.sayOwner()
     var expected: Character = Cristiano
     assertEquals(dueño, expected)
   }
 
   test("NotEqualsOwner") {
-    var dueño1: Character = Sword.sayOwner(Cristiano)
-    var dueño2: Character = Bow.sayOwner(Messi)
+    var dueño1: Character = Sword.sayOwner()
+    var dueño2: Character = Bow.sayOwner()
     assertNotEquals(dueño1, dueño2)
   }
 }
