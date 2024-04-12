ed6da5e22ca69cd32223b7a47fbf43d0393e8222
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-12 14:11:11 -0400

    REFACTOR: edited test 'equalsName','NotequalsName','equalsLife','NotequalsLife','equalsDefence','NotEqualsDefence','equalsWeight','NotEqualsWeight','equalsAttack','NotEqualsAttack' in Test 'EnemyTest'

--- src/test/scala/EnemyTest.scala
+++ src/test/scala/EnemyTest.scala
@@ -2,66 +2,66 @@ import munit.FunSuite
 import model.nonplayable.{MagicWeapon, NonPlayable, Weapon,Enemy}
 import model.playable.{Character, MagicCharacter, Playable}
 class EnemyTest extends FunSuite {
-  var Neymar: Enemy = _
-  var Bale: Enemy = _
+  var Neymar: NonPlayable = _
+  var Bale: NonPlayable = _
   override def beforeEach(context: BeforeEach): Unit = {
-    Neymar = new Enemy()
-    Bale = new Enemy()
+    Neymar = new Enemy("Neymar",60,90,70,30)
+    Bale = new Enemy("Bale",70,85,75,50)
 
   }
 
   test("equalsEnemyName"){
-    var nombre: String = Neymar.sayName("Neymar")
+    var nombre: String = Neymar.sayName()
     var expected: String = "Neymar"
     assertEquals(nombre,expected)
   }
   test("NotEqualsEnemyName"){
-    var nombre1: String = Neymar.sayName("Neymar")
-    var nombre2: String = Bale.sayName("Bale")
+    var nombre1: String = Neymar.sayName()
+    var nombre2: String = Bale.sayName()
     assertNotEquals(nombre1,nombre2)
   }
   test("equalsWeight") {
-    var peso = Neymar.sayWeight(5)
-    var expected: Int = 5
+    var peso = Neymar.sayWeight()
+    var expected: Int = 60
     assertEquals(peso, expected)
   }
   test("NotEqualsWeight") {
-    var peso1 = Neymar.sayWeight(5)
-    var peso2 = Bale.sayWeight(3)
+    var peso1 = Neymar.sayWeight()
+    var peso2 = Bale.sayWeight()
     assertNotEquals(peso1, peso2)
   }
   test("equalsAttack") {
-    var ataque = Neymar.sayAttack(25)
-    var expected: Int = 25
+    var ataque = Neymar.sayAttack()
+    var expected: Int = 90
     assertEquals(ataque, expected)
 
   }
   test("NotEqualsAttack") {
-    var vida1 = Neymar.sayAttack(25)
-    var vida2 = Bale.sayAttack(20)
+    var vida1 = Neymar.sayAttack()
+    var vida2 = Bale.sayAttack()
     assertNotEquals(vida1, vida2)
 
   }
   test("equalsLife") {
-    var vida = Neymar.sayLife(100)
-    var expected: Int = 100
+    var vida = Neymar.sayLife()
+    var expected: Int = 70
     assertEquals(vida, expected)
 
   }
   test("NotEqualsLife") {
-    var vida1 = Neymar.sayLife(100)
-    var vida2 = Bale.sayLife(90)
+    var vida1 = Neymar.sayLife()
+    var vida2 = Bale.sayLife()
     assertNotEquals(vida1, vida2)
 
   }
   test("equalsDefence") {
-    var defensa = Neymar.sayDefence(10)
-    var expected: Int = 10
+    var defensa = Neymar.sayDefence()
+    var expected: Int = 30
     assertEquals(defensa, expected)
   }
   test("NotEqualsDefence") {
-    var defensa1 = Neymar.sayDefence(10)
-    var defensa2 = Bale.sayDefence(5)
+    var defensa1 = Neymar.sayDefence()
+    var defensa2 = Bale.sayDefence()
     assertNotEquals(defensa1, defensa2)
   }
 }
