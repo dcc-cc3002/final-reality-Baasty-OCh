0e2f6448b9244c0238a3bebe963484f4c13f509d
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-12 11:24:23 -0400

    REFACTOR: added methods 'sayName', 'sayLife','sayDefence', 'sayWeight', 'sayKind', 'putWeapon' and edited 'sayMana' in class 'MagicCharacter' in package 'playable'

--- src/main/scala/model/playable/MagicCharacter.scala
+++ src/main/scala/model/playable/MagicCharacter.scala
@@ -1,7 +1,20 @@
 package model.playable
 import model.playable.Character
-class MagicCharacter extends Character {
-  def sayMana(mana: Int): Int = {
-    return mana
-  }
+class MagicCharacter(name:String, life:Int,
+                      defence:Int,weight:Int,
+                      kind:String,weapon:Boolean,
+                      mana:Int) extends Playable {
+  def sayName() = {name}
+  def sayLife()= {life}
+
+  def sayDefence()= {defence}
+
+  def sayWeight()={weight}
+  def sayKind()={kind}
+
+  def havWeapon()={weapon}
+
+  def sayMana()={mana}
+
+
 }
