1759fe3616c7799708cdbfbad0eeb7612c4d4102
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-12 21:23:24 -0400

    FEAT: added var Team1 in PartyTest class

--- src/test/scala/PartyTest.scala
+++ src/test/scala/PartyTest.scala
@@ -1,3 +1,10 @@
-class PartyTest {
+import munit.FunSuite
+import model.party.Party
+class PartyTest extends FunSuite {
+  var Team1: Party = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    var Team1 = new Party()
+  }
 
 }
