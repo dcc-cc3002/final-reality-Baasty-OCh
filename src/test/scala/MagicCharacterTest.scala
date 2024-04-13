9736143cbe1b16b10cce3b1b9f7a7b5431ea5dec
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-12 20:41:02 -0400

    TEST: added test 'equalsMana','NotEqualsMana', 'equalsMagicKind', 'NotEqualsMagicKind','equalsMagicCharacter','NotEqualsMagicCharacter' in class 'MagicCharacterTest'

--- src/test/scala/MagicCharacterTest.scala
+++ src/test/scala/MagicCharacterTest.scala
@@ -1,3 +1,43 @@
-class MagicCharacterTest {
+import model.playable.{Character, MagicCharacter}
+import munit.FunSuite
+class MagicCharacterTest extends FunSuite {
+  var Alexis: MagicCharacter = _
+  var Vidal: MagicCharacter = _
 
+  override def beforeEach(context: BeforeEach): Unit = {
+    Alexis = new MagicCharacter("Alexis", 80, 55, 65, "Mago Blanco", false, 90)
+    Vidal = new MagicCharacter("Vidal", 90, 100, 70, "Mago Negro", true, 50)
+
+  }
+
+  test("equalsMana") {
+    var mana = Alexis.sayMana()
+    var expected: Int = 90
+    assertEquals(mana, expected)
+  }
+  test("NotEqualsMana") {
+    var mana1 = Alexis.sayMana()
+    var mana2 = Vidal.sayMana()
+    assertNotEquals(mana1, mana2)
+  }
+  test("equalsMagicKind") {
+    var clase = Alexis.sayKind()
+    var expected: String = "Mago Blanco"
+    assertEquals(clase, expected)
+  }
+  test("NotEqualsMagicKind") {
+    var clase1 = Alexis.sayKind()
+    var clase2 = Vidal.sayKind()
+    assertNotEquals(clase1, clase2)
+  }
+  test("equalsMagicCharacter") {
+    val pjm: MagicCharacter = Alexis
+    var expected = Alexis
+    assertEquals(pjm, expected)
+  }
+  test("NotEqualsMagicCharacter") {
+    val pjm1: MagicCharacter = Alexis
+    val pjm2: MagicCharacter = Vidal
+    assertNotEquals(pjm1, pjm2)
+  }
 }
--- src/test/scala/PlayableTest.scala
+++ src/test/scala/PlayableTest.scala
@@ -3,13 +3,10 @@ import model.playable.{Character, MagicCharacter, Playable}
 class CharacterTest extends FunSuite {
   var Cristiano: Character = _
   var Messi: Character = _
-  var Alexis: MagicCharacter = _
-  var Vidal: MagicCharacter = _
+
   override def beforeEach(context: BeforeEach): Unit = {
     Cristiano = new Character("Cristiano",100,50,70,"Paladin",false)
     Messi = new Character("Messi",90,40,60,"Guerrero",true)
-    Alexis = new MagicCharacter("Alexis",80,55,65,"Mago Blanco",false,90)
-    Vidal = new MagicCharacter("Vidal",90,100,70,"Mago Negro",true,50)
 
   }
   test("equals") {
@@ -77,34 +74,4 @@ class CharacterTest extends FunSuite {
     assertNotEquals(clase1, clase2)
   }
 
-  test("equalsMana") {
-    var mana = Alexis.sayMana()
-    var expected: Int = 90
-    assertEquals(mana, expected)
-  }
-  test("NotEqualsMana") {
-    var mana1 = Alexis.sayMana()
-    var mana2 = Vidal.sayMana()
-    assertNotEquals(mana1, mana2)
-  }
-  test("equalsMagicKind") {
-    var clase = Alexis.sayKind()
-    var expected: String = "Mago Blanco"
-    assertEquals(clase, expected)
-  }
-  test("NotEqualsMagicKind") {
-    var clase1 = Alexis.sayKind()
-    var clase2 = Vidal.sayKind()
-    assertNotEquals(clase1, clase2)
-  }
-  test("equalsMagicCharacter") {
-    val pjm: MagicCharacter = Alexis
-    var expected = Alexis
-    assertEquals(pjm, expected)
-  }
-  test("NotEqualsMagicCharacter") {
-    val pjm1: MagicCharacter = Alexis
-    val pjm2 : MagicCharacter = Vidal
-    assertNotEquals(pjm1, pjm2)
-  }
 }
