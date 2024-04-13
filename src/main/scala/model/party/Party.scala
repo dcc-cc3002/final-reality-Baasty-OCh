59577b6e239b788b4390741fef5e98308bd6f266
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-12 22:45:30 -0400

    DOC: Document all my code of class 'Party' in English.

--- src/main/scala/model/party/Party.scala
+++ src/main/scala/model/party/Party.scala
@@ -1,21 +1,27 @@
 package model.party
+
 import model.playable.{Character, MagicCharacter, Playable}
 
 import scala.collection.mutable.Map
+
+// Party class to manage a group of playable characters
 class Party {
+  // Map to store the allies in the party, keyed by their kind/type
   val allies: Map[String, Playable] = Map()
 
-  // Método para añadir un personaje a la party
+  // Method to add a playable character to the party
   def addPlayable(pj: Playable): Unit = {
-    allies.put(pj.sayKind(), pj)
-  }
-  // Método para verificar si la party está derrotada
-  def isDefeated: Boolean = if (allies.isEmpty) {
-    println("Party Empty")
-    true // La party está vacía, por lo tanto se considera derrotada
-  } else {
-    allies.values.forall(_.sayLife() == 0)
+    allies.put(pj.sayKind(), pj) // Adds the playable character to the allies map with their kind as the key
   }
 
-
+  // Method to check if the party is defeated
+  def isDefeated: Boolean = {
+    if (allies.isEmpty) {
+      println("Party Empty") // Print a message if the party is empty
+      true // The party is considered defeated if it's empty
+    } else {
+      // The party is considered defeated if all allies have 0 life points
+      allies.values.forall(_.sayLife() == 0)
+    }
+  }
 }
