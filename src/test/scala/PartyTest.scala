8d56dc9dad3c9e90568614a51a4ececeee2b1148
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-12 21:35:14 -0400

    TEST: added test 'addPersonaje' in class 'PartyTest'

--- src/test/scala/PartyTest.scala
+++ src/test/scala/PartyTest.scala
@@ -1,10 +1,10 @@
 import munit.FunSuite
 import model.party.Party
-import model.playable.{Character,MagicCharacter}
+import model.playable.{Character,MagicCharacter,Playable}
 import model.nonplayable.{Enemy,Weapon,MagicWeapon}
 class PartyTest extends FunSuite {
   var Team1: Party = _
-  var Cristiano: Character = _
+  var Cristiano: Playable = _
   var Alexis: MagicCharacter = _
 
   override def beforeEach(context: BeforeEach): Unit = {
@@ -12,6 +12,11 @@ class PartyTest extends FunSuite {
     Cristiano = new Character("Cristiano",100,50,70,"Paladin",false)
     Alexis = new MagicCharacter("Alexis", 80, 55, 65, "Mago Blanco", false, 90)
 
+  }
+  test("addPersonaje") {
+    Team1.addPlayable(Cristiano)
+    val expected: Map[String, Playable] = Map("Paladin" -> Cristiano)
+    assertEquals(Team1.allies.toMap, expected)
   }
 
 
