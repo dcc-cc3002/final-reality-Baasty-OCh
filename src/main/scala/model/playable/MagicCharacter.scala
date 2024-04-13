a2ebacc2803b10d8660e5d7cc33a7958e9b21e33
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-12 22:43:50 -0400

    DOC: Document all my code of class 'MagicCharacter' in English.

--- src/main/scala/model/playable/MagicCharacter.scala
+++ src/main/scala/model/playable/MagicCharacter.scala
@@ -1,40 +1,52 @@
 package model.playable
 import model.playable.Character
-class MagicCharacter(name:String, life:Int,
-                      defence:Int,weight:Int,
-                      kind:String,weapon:Boolean,
-                      mana:Int) extends Playable {
+
+// MagicCharacter class extends the Playable trait
+class MagicCharacter(name: String, life: Int,
+                     defence: Int, weight: Int,
+                     kind: String, weapon: Boolean,
+                     mana: Int) extends Playable {
+
+  // Method to return the name of the magic character
   def sayName(): String = {
     name
   }
 
+  // Method to return the life points of the magic character
   def sayLife(): Int = {
     life
   }
 
+  // Method to return the defense points of the magic character
   def sayDefence(): Int = {
     defence
   }
 
+  // Method to return the weight of the magic character
   def sayWeight(): Int = {
     weight
   }
-  def sayKind():String={kind}
 
+  // Method to return the kind/type of the magic character
+  def sayKind(): String = {
+    kind
+  }
+
+  // Method to check if the magic character has a weapon
   def havWeapon(): Boolean = {
     weapon
   }
 
-  def sayMana()={
+  // Method to return the mana points of the magic character
+  def sayMana(): Int = {
     mana
   }
+
+  // Override equals method to compare two magic characters based on their name and kind
   override def equals(other: Any): Boolean = {
     if (other.isInstanceOf[Playable]) {
       val otherCast = other.asInstanceOf[MagicCharacter]
-      sayName() == otherCast.sayName() &&
-        sayKind() == otherCast.sayKind()
+      sayName() == otherCast.sayName() && sayKind() == otherCast.sayKind()
     } else false
   }
-
-
 }
