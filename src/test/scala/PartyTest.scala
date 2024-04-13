e071223481925de6333267300d881a43d9f42507
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-12 21:26:46 -0400

    FEAT: added var 'Cristiano' and ' Alexis' in PartyTest class

--- src/test/scala/PartyTest.scala
+++ src/test/scala/PartyTest.scala
@@ -1,10 +1,18 @@
 import munit.FunSuite
 import model.party.Party
+import model.playable.{Character,MagicCharacter}
+import model.nonplayable.{Enemy,Weapon,MagicWeapon}
 class PartyTest extends FunSuite {
   var Team1: Party = _
+  var Cristiano: Character = _
+  var Alexis: MagicCharacter = _
 
   override def beforeEach(context: BeforeEach): Unit = {
-    var Team1 = new Party()
+    Team1 = new Party()
+    Cristiano = new Character("Cristiano",100,50,70,"Paladin",false)
+    Alexis = new MagicCharacter("Alexis", 80, 55, 65, "Mago Blanco", false, 90)
+
   }
+
 
 }
