313d2edcd83a2e8a1a7e5627746178483e58903f
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-22 21:13:32 -0400

    DOC: I created class Controller

---
+++ src/main/scala/model/controller/Controller.scala
@@ -0,0 +1,12 @@
+package model.controller
+import model.playable.{Character, MagicCharacter, Playable}
+import model.nonplayable.{MagicWeapon, Weapon}
+import scala.collection.mutable.Map
+import model.party.Party
+
+class Controller {
+
+
+
+}
+
