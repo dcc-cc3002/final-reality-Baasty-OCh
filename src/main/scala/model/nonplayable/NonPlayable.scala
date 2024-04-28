9c02c8fae1e049a21d0ba350aa0d350d7a59e5aa
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-28 13:56:36 -0400

    FEAT: changed methos in trait 'NonPlayable' for 'getters' like in trait 'Playable'

--- src/main/scala/model/nonplayable/NonPlayable.scala
+++ src/main/scala/model/nonplayable/NonPlayable.scala
@@ -2,13 +2,18 @@ package model.nonplayable
 
 // Trait to represent non-playable entities
 trait NonPlayable {
-  // Abstract method to get the name of the non-playable entity
-  def sayName(): String
+  /**
+   * Abstract method to get the name of the non-playable entity
+   */
+  def getName: String
+  /**
+   * Abstract method to get the Weight of the non-playable entity
+   */
+  def getWeight: Int
 
-  // Abstract method to get the weight of the non-playable entity
-  def sayWeight(): Int
-
-  // Abstract method to get the attack power of the non-playable entity
-  def sayAttack(): Int
+  /**
+   * Abstract method to get the Attack Points of the non-playable entity
+   */
+  def getAttack: Int
 }
 
