e421e8846307df6ae8208d1632a970fa8689ce92
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-28 14:37:21 -0400

    REFACTOR: changed class 'Weapon' for abstract class 'AWeapon'

--- src/main/scala/model/nonplayable/AWeapon.scala
+++ src/main/scala/model/nonplayable/AWeapon.scala
@@ -2,8 +2,12 @@ package model.nonplayable
 import model.nonplayable.ANonPlayable
 import model.playable.Playable
 
-// Weapon class represents a non-playable entity that is a weapon
-class Weapon(name: String, weight: Int,
+
+abstract class AWeapon(name: String, weight: Int,
              attack: Int, owner: Playable) extends ANonPlayable (name,weight,attack) {
 
 }
+
+
+
+
