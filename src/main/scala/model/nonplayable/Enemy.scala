07522c8a5b23e4dbb2e74a950de73a7cbf167671
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-28 14:28:48 -0400

    FEAT: created the auxiliary constructor for 'Enemy' class

--- src/main/scala/model/nonplayable/Enemy.scala
+++ src/main/scala/model/nonplayable/Enemy.scala
@@ -1,9 +1,20 @@
 package model.nonplayable
 import model.nonplayable.NonPlayable
 
-// Enemy class represents a non-playable entity that is an enemy
+/**
+ * "An enemy is a type of non-playable character that represents the adversary
+ *  or adversaries to be defeated by the group of players."
+ * @param name
+ * @param weight
+ * @param attack
+ * @param life
+ * @param defence
+ */
 class Enemy(name: String, weight: Int,
             attack: Int, life: Int,
             defence: Int) extends ANonPlayable(name,weight,attack) {
+  def this(name:String) ={
+    this(name,50,50,250,50)
+  }
 
 }
