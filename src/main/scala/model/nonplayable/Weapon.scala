a5e443fb2303d6bc3bc7af5ab5971aa68549d0a1
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-28 14:31:14 -0400

    FEAT: eliminated all methods in class 'Weapon' because there were implement in abstrac class 'ANonPlayable', also changed the type of @owner for 'Playable'

--- src/main/scala/model/nonplayable/Weapon.scala
+++ src/main/scala/model/nonplayable/Weapon.scala
@@ -1,28 +1,9 @@
 package model.nonplayable
-import model.nonplayable.NonPlayable
-import model.playable.APlayable
+import model.nonplayable.ANonPlayable
+import model.playable.Playable
 
 // Weapon class represents a non-playable entity that is a weapon
 class Weapon(name: String, weight: Int,
-             attack: Int, owner: Character) extends NonPlayable {
+             attack: Int, owner: Playable) extends ANonPlayable (name,weight,attack) {
 
-  // Method to return the owner character of the weapon
-  def sayOwner(): Character = {
-    owner
-  }
-
-  // Method to return the name of the weapon
-  def sayName(): String = {
-    name
-  }
-
-  // Method to return the weight of the weapon
-  def sayWeight(): Int = {
-    weight
-  }
-
-  // Method to return the attack power of the weapon
-  def sayAttack(): Int = {
-    attack
-  }
 }
