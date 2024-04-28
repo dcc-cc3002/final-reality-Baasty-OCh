681fddacf4ff882f4ce89edf9f9cedb7a755779b
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-04-28 14:05:27 -0400

    FEAT: added implementation for methods 'getName', 'getWeight' and 'getAttack' from trait 'NonPlayble' in  abstract class 'ANonPlayable', also added method equals for these class 'NonPlayable'

--- src/main/scala/model/nonplayable/ANonPlayable.scala
+++ src/main/scala/model/nonplayable/ANonPlayable.scala
@@ -1,5 +1,33 @@
 package model.nonplayable
+import model.nonplayable.NonPlayable
 
-class ANonPlayable {
-
+/**
+ *
+ * @param name
+ * @param weight
+ * @param attackPoints
+ */
+abstract class ANonPlayable(val name: String, val weight: Int,
+                   val attackPoints: Int) extends NonPlayable {
+  /**
+   * Implementation of Method to get the name of the non-playable entity
+   * @return : The name of non-playable entity
+   */
+  def getName: String = name
+  /**
+   * Implementation of Method to get the weight of the non-playable entity
+   * @return The weight of non-playable entity
+   */
+  def getWeight: Int = weight
+  /**
+   * Implementation of Method to get the attack points of the non-playable entity
+   * @return The attack points of non-playable entity
+   */
+  def getAttack: Int = attackPoints
+  override def equals(other:Any):Boolean = { // override anula la funcion por defecto y me permite escribir una nueva
+    if (other.isInstanceOf[NonPlayable]) {
+      val otherCast = other.asInstanceOf[NonPlayable]
+      getClass() == otherCast.getClass()
+    } else false
+  }
 }
