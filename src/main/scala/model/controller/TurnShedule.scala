2515e427b85d56b7dad9035bdbdfba1994827b2c
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-02 15:25:47 -0400

    FEAT: added method 'addPlayable' in class 'TurnShedule'

--- src/main/scala/model/controller/TurnShedule.scala
+++ src/main/scala/model/controller/TurnShedule.scala
@@ -1,9 +1,23 @@
 package model.controller
 
 import model.playable.Playable
+import model.playable.APlayable
+import scala.collection.mutable.Map
 
 
 class TurnShedule extends Shedule {
+  var actionBar : Map[Int, Playable] = Map()
+  def addPlayable( pj :Playable): Unit = {
+    actionBar += (pj -> 0)
+  }
+
+  def maxActionBar(): Unit = {
+
+  }
+
+
+
+
 
 
 
