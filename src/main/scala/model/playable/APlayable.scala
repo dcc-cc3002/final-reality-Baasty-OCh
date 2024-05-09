96b689af6d40c352d72ea04b0d648ddb5f1e8059
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-09 14:17:05 -0400

    FEAT: added documentation for all methods in 'APlayable' class

--- src/main/scala/model/playable/APlayable.scala
+++ src/main/scala/model/playable/APlayable.scala
@@ -75,14 +75,18 @@ abstract class APlayable(val name: String, var healthPoints: Int,
   }
 
   /**
-   * Implementation of Method to hurt an Playable Entity
-   * @param pain
-   *  @return a new Playable
+   * Method to simulate the playable entity being attacked.
+   * @param pain The amount of damage inflicted on the playable entity.
+   *  @return A new `Playable` instance representing the entity after being hurt.
    */
   def wasAttacked(pain:Int) : Playable
 
-
-
+  /**
+   * Custom implementation of the `equals` method to compare objects for class equality.
+   * This method overrides the default `equals` method inherited from the `Any` class.
+   * @param other The object to compare with.
+   * @return `true` if the current object's class is the same as `other`'s class (if `other` is an instance of `Playable`), otherwise `false`.
+   */
   override def equals(other:Any):Boolean = { // override anula la funcion por defecto y me permite escribir una nueva
     if (other.isInstanceOf[Playable]) {
       val otherCast = other.asInstanceOf[Playable]
