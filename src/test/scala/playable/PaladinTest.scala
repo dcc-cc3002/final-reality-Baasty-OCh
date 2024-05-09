18fc293ff28a0c989d17c1296f0e819f601cdfcf
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-09 14:31:47 -0400

    TEST: added test 'equals' and 'getName' in class 'PaladinTest'

--- src/test/scala/playable/PaladinTest.scala
+++ src/test/scala/playable/PaladinTest.scala
@@ -1,5 +1,45 @@
 package playable
+import model.nonplayable.{AWeapon, Axe, Bow, Enemy, NonPlayable, Staff, Sword, Wand}
+import model.playable.Playable
+import model.playable.{APlayable, Warrior, Ninja, Paladin}
+import munit.FunSuite
+/**
+ * Test suite for the `Paladin` class.
+ * This class contains tests for various functionalities of the `Paladin` class.
+ */
+class PaladinTest extends FunSuite {
+  var Cristiano: Playable = _ // Declaration of a variable `Cristiano` of type `Playable`.
+  var Pele: Playable = _ // Declaration of a variable `Pele` of type `Playable`.
 
-class PaladinTest {
+  /**
+   * Set up the test environment before each test case.
+   * This method is invoked before each test case (`test`) to initialize the test objects.
+   * @param context The context provided by the test framework (`BeforeEach`).
+   */
+  override def beforeEach(context: BeforeEach): Unit = {
+    // Initialize `Cristiano` and `Pele` with new instances of `Paladin`.
+    Cristiano = new Paladin("Cristiano")
+    Pele = new Paladin("Pele", 30, 50, 90, 0)
+  }
 
+  /**
+   * Test case for `equals` method.
+   * This test verifies if two references to the same character (`Cristiano`) are considered equal.
+   */
+  test("equals") {
+    assertEquals(Cristiano, Cristiano)
+  }
+
+  /**
+   * Test case for `getName` method.
+   * This test verifies if the returned name value for a character (`Cristiano`) matches the expected value.
+   */
+  test("getName") {
+    // Retrieve the name value of the character `Cristiano` and store it in the `name` variable.
+    var name = Cristiano.getName
+    // Compare the actual name value (`name`) with the expected value ("Cristiano").
+    assertEquals(name, "Cristiano")
+  }
 }
+
+
