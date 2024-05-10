6dd1494cf6060b1901dcc7b209381c3db14cef05
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-10 11:31:00 -0400

    DOC: added documentation to class 'WandTest'

--- src/test/scala/nonplayable/weapons/StaffTest.scala
+++ src/test/scala/nonplayable/weapons/StaffTest.scala
@@ -1,13 +1,18 @@
 package nonplayable.weapons
-import model.nonplayable.weapons.Sword
+
+import model.nonplayable.weapons.Staff
 import model.nonplayable.NonPlayable
 import model.playable.Playable
 import model.nonplayable.weapons.Bow
 import munit.FunSuite
 
-class StaffTest extends FunSuite{
-  var Espada: Bow = _ // Declaration of a variable `Espada` of type `Bow`.
-  var Cristiano: Playable = _ // Declaration of a variable `Cristiano` of type `Playable`.
+/**
+ * Test suite for the `Staff` class.
+ * This class contains tests for various functionalities of the `Staff` class.
+ */
+class StaffTest extends FunSuite {
+  var Baston: Bow = _ // Declaration of a variable `Baston` of type `Bow`.
+  var Benzema: Playable = _ // Declaration of a variable `Benzema` of type `Playable`.
 
   /**
    * Set up the test environment before each test case.
@@ -17,35 +22,35 @@ class StaffTest extends FunSuite{
    */
   override def beforeEach(context: BeforeEach): Unit = {
     // Creates a new instance of the `Bow` class with the specified parameters:
-    Espada = new Bow(Cristiano) // - Name: Espada // - Weight: 25 // - Attack: 50 // - Owner: Modric
+    Baston = new Bow(Benzema) // - Name: Baston // - Weight: 25 // - Attack: 50 // - Owner: Modric
   }
 
   /**
    * Test case for `equals` method.
-   * This test verifies if two references to the same sword (`Espada`) are considered equal.
+   * This test verifies if two references to the same staff (`Baston`) are considered equal.
    */
   test("equals") {
-    assertEquals(Espada, Espada)
+    assertEquals(Baston, Baston)
   }
 
   /**
    * Test case for `getName` method.
-   * This test verifies if the returned name value for a sword (`Espada`) matches the expected value.
+   * This test verifies if the returned name value for a staff (`Baston`) matches the expected value.
    */
   test("getName") {
-    // Retrieve the name value of the sword `Espada` and store it in the `name2` variable.
-    var name2 = Espada.getName
-    // Compare the actual name value (`name2`) with the expected value ("Espada").
-    assertEquals(name2, "Espada")
+    // Retrieve the name value of the staff `Baston` and store it in the `name2` variable.
+    var name2 = Baston.getName
+    // Compare the actual name value (`name2`) with the expected value ("Baston").
+    assertEquals(name2, "Baston")
   }
 
   /**
    * Test case for `getWeight` method.
-   * This test verifies if the returned weight value for a sword (`Espada`) matches the expected value (`25`).
+   * This test verifies if the returned weight value for a staff (`Baston`) matches the expected value (`25`).
    */
   test("getWeight") {
-    // Retrieve the weight value of the sword `Espada` and store it in the `weight` variable.
-    var weight = Espada.getWeight
+    // Retrieve the weight value of the staff `Baston` and store it in the `weight` variable.
+    var weight = Baston.getWeight
     // Define the expected weight value.
     var expected: Int = 25
     // Compare the actual weight value (`weight`) with the expected value (`expected`).
@@ -54,11 +59,11 @@ class StaffTest extends FunSuite{
 
   /**
    * Test case for `getAttack` method.
-   * This test verifies if the returned attack value for a sword (`Espada`) matches the expected value (`50`).
+   * This test verifies if the returned attack value for a staff (`Baston`) matches the expected value (`50`).
    */
   test("getAttack") {
-    // Retrieve the attack value of the sword `Espada` and store it in the `attack` variable.
-    var attack = Espada.getAttack
+    // Retrieve the attack value of the staff `Baston` and store it in the `attack` variable.
+    var attack = Baston.getAttack
     // Define the expected attack value.
     var expected: Int = 50
     // Compare the actual attack value (`attack`) with the expected value (`expected`).
@@ -67,22 +72,23 @@ class StaffTest extends FunSuite{
 
   /**
    * Test case for `getOwner` method.
-   * This test verifies if the returned owner character for a sword (`Espada`) matches the expected character (`Cristiano`).
+   * This test verifies if the returned owner character for a staff (`Baston`) matches the expected character (`Benzema`).
    */
   test("getOwner") {
-    // Retrieve the owner character of the sword `Espada` and store it in the `owner` variable.
-    var owner: Any = Espada.getOwner
+    // Retrieve the owner character of the staff `Baston` and store it in the `owner` variable.
+    var owner: Any = Baston.getOwner
     // Define the expected owner character.
-    var expected: Playable = Cristiano
+    var expected: Playable = Benzema
     // Compare the actual owner character (`owner`) with the expected character (`expected`).
     assertEquals(owner, expected)
   }
 
   /**
    * Test case for `getMAP` method.
-   * This test verifies if the returned magic attack points value for a sword (`Espada`) matches the expected value (`0`).
+   * This test verifies if the returned magic attack points value for a staff (`Baston`) matches the expected value (`0`).
    */
   test("getMAP") {
-    assertEquals(Espada.getMAP, 0)
+    assertEquals(Baston.getMAP, 0)
   }
 }
+
--- src/test/scala/nonplayable/weapons/WandTest.scala
+++ src/test/scala/nonplayable/weapons/WandTest.scala
@@ -1,5 +1,94 @@
 package nonplayable.weapons
 
-class WandTest {
+import model.nonplayable.weapons.Wand
+import model.nonplayable.NonPlayable
+import model.playable.Playable
+import model.nonplayable.weapons.Bow
+import munit.FunSuite
+
+/**
+ * Test suite for the `Wand` class.
+ * This class contains tests for various functionalities of the `Wand` class.
+ */
+class WandTest extends FunSuite {
+  var Barita: Bow = _ // Declaration of a variable `Barita` of type `Bow`.
+  var Modric: Playable = _ // Declaration of a variable `Modric` of type `Playable`.
+
+  /**
+   * Set up the test environment before each test case.
+   * This method is invoked before each test case (`test`) to initialize the test objects.
+   *
+   * @param context The context provided by the test framework (`BeforeEach`).
+   */
+  override def beforeEach(context: BeforeEach): Unit = {
+    // Creates a new instance of the `Bow` class with the specified parameters:
+    Barita = new Bow(Modric) // - Name: Barita // - Weight: 25 // - Attack: 50 // - Owner: Modric
+  }
+
+  /**
+   * Test case for `equals` method.
+   * This test verifies if two references to the same wand (`Barita`) are considered equal.
+   */
+  test("equals") {
+    assertEquals(Barita, Barita)
+  }
+
+  /**
+   * Test case for `getName` method.
+   * This test verifies if the returned name value for a wand (`Barita`) matches the expected value.
+   */
+  test("getName") {
+    // Retrieve the name value of the wand `Barita` and store it in the `name2` variable.
+    var name2 = Barita.getName
+    // Compare the actual name value (`name2`) with the expected value ("Barita").
+    assertEquals(name2, "Barita")
+  }
+
+  /**
+   * Test case for `getWeight` method.
+   * This test verifies if the returned weight value for a wand (`Barita`) matches the expected value (`25`).
+   */
+  test("getWeight") {
+    // Retrieve the weight value of the wand `Barita` and store it in the `weight` variable.
+    var weight = Barita.getWeight
+    // Define the expected weight value.
+    var expected: Int = 25
+    // Compare the actual weight value (`weight`) with the expected value (`expected`).
+    assertEquals(weight, expected)
+  }
+
+  /**
+   * Test case for `getAttack` method.
+   * This test verifies if the returned attack value for a wand (`Barita`) matches the expected value (`50`).
+   */
+  test("getAttack") {
+    // Retrieve the attack value of the wand `Barita` and store it in the `attack` variable.
+    var attack = Barita.getAttack
+    // Define the expected attack value.
+    var expected: Int = 50
+    // Compare the actual attack value (`attack`) with the expected value (`expected`).
+    assertEquals(attack, expected)
+  }
+
+  /**
+   * Test case for `getOwner` method.
+   * This test verifies if the returned owner character for a wand (`Barita`) matches the expected character (`Modric`).
+   */
+  test("getOwner") {
+    // Retrieve the owner character of the wand `Barita` and store it in the `owner` variable.
+    var owner: Any = Barita.getOwner
+    // Define the expected owner character.
+    var expected: Playable = Modric
+    // Compare the actual owner character (`owner`) with the expected character (`expected`).
+    assertEquals(owner, expected)
+  }
+
+  /**
+   * Test case for `getMAP` method.
+   * This test verifies if the returned magic attack points value for a wand (`Barita`) matches the expected value (`0`).
+   */
+  test("getMAP") {
+    assertEquals(Barita.getMAP, 0)
+  }
 
 }
