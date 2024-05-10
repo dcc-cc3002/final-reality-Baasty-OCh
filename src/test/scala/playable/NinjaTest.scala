8cd87b4a2ff4d8c87d2e9f3e6b8db4c02541a185
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-10 10:11:22 -0400

    TEST: delete class 'MagicCharacterTest' since were useless

--- src/test/scala/playable/NinjaTest.scala
+++ src/test/scala/playable/NinjaTest.scala
@@ -1,14 +1,16 @@
 package playable
+
 import model.nonplayable.{AWeapon, Axe, Bow, Enemy, NonPlayable, Staff, Sword, Wand}
 import model.playable.Playable
 import model.playable.{APlayable, Warrior, Ninja, Paladin}
 import munit.FunSuite
+
 /**
  * Test suite for the `Paladin` class.
  * This class contains tests for various functionalities of the `Paladin` class.
  */
 class NinjaTest extends FunSuite {
-  var Modric: Playable = _ // Declaration of a variable `Modric` of type `Playable`.
+  var Kroos: Playable = _ // Declaration of a variable `Kroos` of type `Playable`.
   var Rakitic: Enemy = _ // Declaration of a variable `Rakitic` of type `Enemy`.
 
   /**
@@ -18,114 +20,125 @@ class NinjaTest extends FunSuite {
    * @param context The context provided by the test framework (`BeforeEach`).
    */
   override def beforeEach(context: BeforeEach): Unit = {
-    // Initialize `Modric` with new instances of `Ninja`.
-    Modric = new Ninja("Modric")
+    // Initialize `Kroos` with new instances of `Ninja`.
+    Kroos = new Ninja("Kroos")
     // Initialize `Rakitic` with new instances of `Enemy`.
     Rakitic = new Enemy("Rakitic")
   }
+
   /**
    * Test case for `equals` method.
-   * This test verifies if two references to the same character (`Modric`) are considered equal.
+   * This test verifies if two references to the same character (`Kroos`) are considered equal.
    */
   test("equals") {
-    assertEquals(Modric, Modric)
+    assertEquals(Kroos, Kroos)
   }
+
   /**
    * Test case for `getName` method.
-   * This test verifies if the returned name value for a character (`Modric`) matches the expected value.
+   * This test verifies if the returned name value for a character (`Kroos`) matches the expected value.
    */
   test("getName") {
-    // Retrieve the name value of the character `Modric` and store it in the `name` variable.
-    var name = Modric.getName
-    // Compare the actual name value (`name`) with the expected value ("Modric").
-    assertEquals(name, "Modric")
+    // Retrieve the name value of the character `Kroos` and store it in the `name` variable.
+    var name = Kroos.getName
+    // Compare the actual name value (`name`) with the expected value ("Kroos").
+    assertEquals(name, "Kroos")
   }
+
   /**
    * Test case for `getHp` method.
-   * This test verifies if the returned health points (`hp`) value for the character `Modric` matches the expected value (`80`).
+   * This test verifies if the returned health points (`hp`) value for the character `Kroos` matches the expected value (`80`).
    */
   test("getHealthPoints") {
-    // Retrieve the health points (`hp`) value of the character `Modric` and store it in the `life` variable.
-    var life = Modric.getHp
+    // Retrieve the health points (`hp`) value of the character `Kroos` and store it in the `life` variable.
+    var life = Kroos.getHp
     // Compare the actual health points value (`life`) with the expected value (`80`).
     assertEquals(life, 80)
   }
+
   /**
    * Test case for `getDp` method.
-   * This test verifies if the returned defense points (`dp`) value for the character `Modric` matches the expected value (`70`).
+   * This test verifies if the returned defense points (`dp`) value for the character `Kroos` matches the expected value (`70`).
    */
   test("getDefencePoints") {
-    // Retrieve the defense points (`dp`) value of the character `Modric` and store it in the `defense` variable.
-    var defense = Modric.getDp
+    // Retrieve the defense points (`dp`) value of the character `Kroos` and store it in the `defense` variable.
+    var defense = Kroos.getDp
     // Compare the actual defense points value (`defense`) with the expected value (`70`).
     assertEquals(defense, 70)
   }
+
   /**
    * Test case for `getWeight` method.
-   * This test verifies if the returned weight value for the character `Modric` matches the expected value (`30`).
+   * This test verifies if the returned weight value for the character `Kroos` matches the expected value (`30`).
    */
   test("getWeight") {
-    // Retrieve the weight value of the character `Modric` and store it in the `weight` variable.
-    var weight = Modric.getWeight
+    // Retrieve the weight value of the character `Kroos` and store it in the `weight` variable.
+    var weight = Kroos.getWeight
     // Compare the actual weight value (`weight`) with the expected value (`30`).
     assertEquals(weight, 30)
   }
+
   /**
    * Test case for `getMana` method.
-   * This test verifies if the returned mana value for the character `Modric` matches the expected value (`0`).
+   * This test verifies if the returned mana value for the character `Kroos` matches the expected value (`0`).
    */
   test("getMana") {
-    // Retrieve the mana value of the character `Modric` and store it in the `mana` variable.
-    var mana = Modric.getMana
+    // Retrieve the mana value of the character `Kroos` and store it in the `mana` variable.
+    var mana = Kroos.getMana
     // Compare the actual mana value (`mana`) with the expected value (`0`).
     assertEquals(mana, 0)
   }
+
   /**
    * Test case for `haveWeapon` method.
-   * This test verifies if the playable entity `Modric` does not have a weapon (returns `None`).
+   * This test verifies if the playable entity `Kroos` does not have a weapon (returns `None`).
    */
   test("HaveWeapon") {
-    assertEquals(Modric.haveWeapon, None)
+    assertEquals(Kroos.haveWeapon, None)
   }
+
   /**
    * Test case for `putWeapon` method.
-   * This test verifies if the `putWeapon` method successfully equips a weapon (`Bow`) on the character `Modric`.
+   * This test verifies if the `putWeapon` method successfully equips a weapon (`Bow`) on the character `Kroos`.
    */
   test("putWeapon") {
-    // Create a new `Bow` instance (`Arco`) associated with `Modric`.
-    var Arco: Bow = new Bow(Modric)
-    // Equip the `Arco` weapon on the character `Modric` using the `putWeapon` method.
-    Modric.putWeapon(Arco)
-    // Verify if the character `Modric` now has the `Arco` weapon equipped (returns `Some(Arco)`).
-    assertEquals(Modric.haveWeapon, Some(Arco))
+    // Create a new `Bow` instance (`Arco`) associated with `Kroos`.
+    var Arco: Bow = new Bow(Kroos)
+    // Equip the `Arco` weapon on the character `Kroos` using the `putWeapon` method.
+    Kroos.putWeapon(Arco)
+    // Verify if the character `Kroos` now has the `Arco` weapon equipped (returns `Some(Arco)`).
+    assertEquals(Kroos.haveWeapon, Some(Arco))
   }
+
   /**
    * Test case for `attackEnemy` method.
-   * This test verifies if the `attackEnemy` method inflicts the correct damage on the enemy (`Rakitic`) when `Modric` attacks with a `Bow`.
+   * This test verifies if the `attackEnemy` method inflicts the correct damage on the enemy (`Rakitic`) when `Kroos` attacks with a `Bow`.
    */
   test("attackEnemy") {
-    // Equip `Modric` with a `Bow`.
-    Modric.putWeapon(new Bow(Modric))
+    // Equip `Kroos` with a `Bow`.
+    Kroos.putWeapon(new Bow(Kroos))
 
-    // `Modric` attacks `Rakitic` using the `attackEnemy` method and retrieves the inflicted damage (`daño`).
-    val damage: Int = Modric.attackEnemy(Rakitic)
+    // `Kroos` attacks `Rakitic` using the `attackEnemy` method and retrieves the inflicted damage (`damage`).
+    val damage: Int = Kroos.attackEnemy(Rakitic)
 
-    // Calculate the expected damage based on `Modric`'s attack power (Bow's attack - `Rakitic`'s defense).
+    // Calculate the expected damage based on `Kroos`'s attack power (Bow's attack - `Rakitic`'s defense).
     val expectedDamage: Int = 40 - 50
 
     // Verify if the inflicted damage (`damage`) matches the expected damage (`expectedDamage`).
     assertEquals(damage, expectedDamage)
   }
+
   /**
    * Test case for `wasAttacked` method.
-   * This test verifies the behavior of the `wasAttacked` method in response to inflicted damage on the character `Modric`.
+   * This test verifies the behavior of the `wasAttacked` method in response to inflicted damage on the character `Kroos`.
    */
   test("wasAttacked") {
-    // Verify if `Modric`'s health points (`getHp`) decrease correctly after receiving 5 damage.
-    assertEquals(Modric.wasAttacked(5).getHp, 75) // 80 health points - 5 damage -> 75 health points
+    // Verify if `Kroos`'s health points (`getHp`) decrease correctly after receiving 5 damage.
+    assertEquals(Kroos.wasAttacked(5).getHp, 75) // 80 health points - 5 damage -> 75 health points
 
-    // Verify if `Modric`'s health points (`getHp`) become 0 after receiving 100 damage (exceeds total health).
-    assertEquals(Modric.wasAttacked(100).getHp, 0) // 80 health points - 100 damage -> 0 health points
+    // Verify if `Kroos`'s health points (`getHp`) become 0 after receiving 100 damage (exceeds total health).
+    assertEquals(Kroos.wasAttacked(100).getHp, 0) // 80 health points - 100 damage -> 0 health points
   }
 }
+
 
