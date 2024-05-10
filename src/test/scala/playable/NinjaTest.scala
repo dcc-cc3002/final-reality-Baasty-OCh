9076748d54301d0733972c73d14d2b0debbd5205
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-10 09:26:45 -0400

    TEST: added method 'beforeEach' in class 'NinjaTest'

--- src/test/scala/playable/NinjaTest.scala
+++ src/test/scala/playable/NinjaTest.scala
@@ -1,5 +1,27 @@
 package playable
+import model.nonplayable.{AWeapon, Axe, Bow, Enemy, NonPlayable, Staff, Sword, Wand}
+import model.playable.Playable
+import model.playable.{APlayable, Warrior, Ninja, Paladin}
+import munit.FunSuite
+/**
+ * Test suite for the `Paladin` class.
+ * This class contains tests for various functionalities of the `Paladin` class.
+ */
+class NinjaTest extends FunSuite {
+  var Modric: Playable = _ // Declaration of a variable `Modric` of type `Playable`.
+  var Rakitic: Enemy = _ // Declaration of a variable `Rakitic` of type `Enemy`.
 
-class NinjaTest {
-
+  /**
+   * Set up the test environment before each test case.
+   * This method is invoked before each test case (`test`) to initialize the test objects.
+   *
+   * @param context The context provided by the test framework (`BeforeEach`).
+   */
+  override def beforeEach(context: BeforeEach): Unit = {
+    // Initialize `Modric` with new instances of `Ninja`.
+    Modric = new Warrior("Modric")
+    // Initialize `Rakitic` with new instances of `Enemy`.
+    Rakitic = new Enemy("Rakitic")
+  }
 }
+
