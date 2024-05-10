4890566a2f4a81fe8f1081c6c6cd682c49195988
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-10 10:59:02 -0400

    DOC: added documentation to class 'Wand'

--- src/main/scala/model/nonplayable/weapons/Wand.scala
+++ src/main/scala/model/nonplayable/weapons/Wand.scala
@@ -2,13 +2,33 @@ package model.nonplayable.weapons
 import model.nonplayable.weapons.AWeapon
 import model.playable.{Warrior, WhiteMagican, BlackMagican, Ninja, Paladin, Playable}
 
-class Wand(name: String, weight: Int,
-           attackPoints: Int, owner: Playable ,
-           magicAttackPoints: Int) extends AWeapon(name, weight, attackPoints, owner, magicAttackPoints) {
-  def this(owner:Playable) = {
-    this("Varita",40,50,owner, 30)
+/**
+ * Class representing a Wand, which is a type of non-playable weapon.
+ *
+ * @param name             The name of the wand
+ * @param weight           The weight of the wand
+ * @param attackPoints     The attack points of the wand
+ * @param owner            The playable entity that owns this wand
+ * @param magicAttackPoints The magic attack points of the wand
+ */
+class Wand(name: String, weight: Int, attackPoints: Int, owner: Playable, magicAttackPoints: Int)
+  extends AWeapon(name, weight, attackPoints, owner, magicAttackPoints) {
+
+  /**
+   * Constructs a Wand with specified parameters and a specified owner.
+   *
+   * @param owner The playable entity that will own this wand
+   */
+  def this(owner: Playable) = {
+    this("Varita", 40, 50, owner, 30)
   }
-  def this()={
-    this("Varita",40,50, new WhiteMagican("Alexis"), 30)
+
+  /**
+   * Constructs a Wand with default parameters and a default owner (WhiteMagican).
+   * This is a convenience constructor for testing purposes.
+   */
+  def this() = {
+    this("Varita", 40, 50, new WhiteMagican("Alexis"), 30)
   }
 }
+
