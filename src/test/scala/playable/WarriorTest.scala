5a64f8d673003d5a2c4cf63484330a9856c107a6
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-10 09:00:38 -0400

    TEST: added test 'getName' in class 'WarriorTest'

--- src/test/scala/playable/WarriorTest.scala
+++ src/test/scala/playable/WarriorTest.scala
@@ -32,4 +32,14 @@ class WarriorTest extends FunSuite {
     assertEquals(Casemiro, Casemiro)
   }
 
+  /**
+   * Test case for `getName` method.
+   * This test verifies if the returned name value for a character (`Cristiano`) matches the expected value.
+   */
+  test("getName") {
+    // Retrieve the name value of the character `Cristiano` and store it in the `name` variable.
+    var name = Casemiro.getName
+    // Compare the actual name value (`name`) with the expected value ("Cristiano").
+    assertEquals(name, "Casemiro")
+  }
 }
