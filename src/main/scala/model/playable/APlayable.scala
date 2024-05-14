753a9ce10cc9eebdb1dcc32b771406d8dc2e7e7f
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-13 21:36:03 -0400

    FEAT: added  method 'setOwner' in implementation of method 'putWeapon' to delegate responsability in the weapon

--- src/main/scala/model/playable/APlayable.scala
+++ src/main/scala/model/playable/APlayable.scala
@@ -82,7 +82,8 @@ protected abstract class APlayable(val name: String, var healthPoints: Int,
    * @param weapon class of AWeapon (any sub-class of abstract class; Sword, Axe, Bow, Wand and Staff)
    */
   def putWeapon(weapon: AWeapon): Unit = {
-    arma = Some(weapon)
+    weapon.setOwner(Some(this)) // aqui es donde el arma debe saber como equiparse en cada personaje
+    this.arma = Some(weapon)
   }
 
   /**
