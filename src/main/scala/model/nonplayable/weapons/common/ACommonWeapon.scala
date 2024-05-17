ed532038cc431ca0d2d7b9f3e52abb284faefeb3
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-17 18:22:36 -0400

    FEAT: added documentation for abstract class 'ACommonWeapon'

--- src/main/scala/model/nonplayable/weapons/common/ACommonWeapon.scala
+++ src/main/scala/model/nonplayable/weapons/common/ACommonWeapon.scala
@@ -2,6 +2,12 @@ package model.nonplayable.weapons.common
 
 import model.nonplayable.weapons.AWeapon
 
+/**
+ * Abstract class to made a group for common (non-magic) weapons
+ * @param name             The name of the weapon
+ * @param weight           The weight of the weapon
+ * @param attackPoints     The attack points of the weapon
+ */
 abstract class ACommonWeapon(name: String, weight: Int,
                              attackPoints: Int) extends AWeapon(name, weight, attackPoints) {
 
