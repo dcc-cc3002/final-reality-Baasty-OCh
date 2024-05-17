c8cdaa00fb72f6f2d298af0eecc0022879d45b9b
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-17 18:37:18 -0400

    FEAT: added documentation for abstract class 'AMagicPlayable'

--- src/main/scala/model/playable/magic/AMagicPlayable.scala
+++ src/main/scala/model/playable/magic/AMagicPlayable.scala
@@ -2,10 +2,19 @@ package model.playable.magic
 
 import model.playable.APlayable
 
+/**
+ * Abstract class to made a group of magic characters
+ * @param name The name of the character.
+ * @param healthPoints The health points of the character.
+ * @param defensePoints The defense points of the character.
+ * @param weight The weight of the character.
+ * @param mana The mana of the character.
+ */
 abstract class AMagicPlayable(name:String, healthPoints:Int,
                               defensePoints:Int, weight:Int,
                               mana:Int) extends APlayable(name,healthPoints, defensePoints,weight) {
   private var Mana: Int = mana
+
   /**
    * Implementation of Method to get the mana points of the playable entity
    * @return The mana of playable entity
