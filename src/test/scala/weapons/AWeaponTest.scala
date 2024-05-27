d39aa511e1d42b6f86029605ec586b2c77425940
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-05-27 18:53:35 -0400

    TEST: added test 'setOwner' in class 'AWeaponTest' also delete all useless test in other weapons test

--- src/test/scala/weapons/AWeaponTest.scala
+++ src/test/scala/weapons/AWeaponTest.scala
@@ -2,7 +2,7 @@ package weapons
 
 import model.playable.Playable
 import model.playable.common.{Ninja, Paladin, Warrior}
-import model.playable.magic.WhiteMagican
+import model.playable.magic.{BlackMagican, WhiteMagican}
 import model.weapons.{AWeapon, Weapon}
 import model.weapons.common.{Axe, Bow, Sword}
 import model.weapons.magic.{Staff, Wand}
@@ -11,7 +11,7 @@ import munit.FunSuite
 class AWeaponTest extends FunSuite{
   var Tango: AWeapon = _
   var Jabulani: AWeapon = _
-  var Cachaña: AWeapon = _
+  var Cachanna: AWeapon = _
   var Azteca: AWeapon = _
   var Telstar: AWeapon = _
 
@@ -25,7 +25,7 @@ class AWeaponTest extends FunSuite{
   override def beforeEach(context: BeforeEach): Unit = {
     Tango = new Sword()
     Jabulani = new Axe()
-    Cachaña = new Bow()
+    Cachanna = new Bow()
     Azteca = new Staff()
     Telstar = new Wand()
 
@@ -33,37 +33,39 @@ class AWeaponTest extends FunSuite{
 
   /**
    * Test case for `getName` method.
-   * This test verifies if the returned name value for sword 'Tango', axe `Jabulani`, bow 'Cachaña',
+   * This test verifies if the returned name value for sword 'Tango', axe `Jabulani`, bow 'Cachanna',
    * staff 'Azteca' and wand 'Telstar' matches the expected value (Espada, Hacha, Arco, Baston, Varita)
    */
   test("getName") {
     assertEquals(Tango.getName, "Espada")
     assertEquals(Jabulani.getName, "Hacha")
-    assertEquals(Cachaña.getName, "Arco")
+    assertEquals(Cachanna.getName, "Arco")
     assertEquals(Azteca.getName, "Baston")
     assertEquals(Telstar.getName, "Varita")
   }
 
   /**
    * Test case for `getWeight` method.
-   * This test verifies if the returned weight value for an axe (`Hacha`) matches the expected value (`25`).
+   * This test verifies if the returned weight value for sword 'Tango', axe `Jabulani`, bow 'Cachanna',
+   * * staff 'Azteca' and wand 'Telstar' matches the expected values (70,50,30,70,40).
    */
   test("getWeight") {
     assertEquals(Tango.getWeight, 70)
     assertEquals(Jabulani.getWeight, 50)
-    assertEquals(Cachaña.getWeight, 30)
+    assertEquals(Cachanna.getWeight, 30)
     assertEquals(Azteca.getWeight, 70)
     assertEquals(Telstar.getWeight, 40)
   }
 
   /**
    * Test case for `getAttack` method.
-   * This test verifies if the returned attack value for an axe (`Hacha`) matches the expected value (`50`).
+   * This test verifies if the returned attack value for sword 'Tango', axe `Jabulani`, bow 'Cachanna',
+   * * staff 'Azteca' and wand 'Telstar' matches the expected values (70,50,40,70,50).
    */
   test("getAttack") {
     assertEquals(Tango.getAttack, 70)
     assertEquals(Jabulani.getAttack, 50)
-    assertEquals(Cachaña.getAttack, 40)
+    assertEquals(Cachanna.getAttack, 40)
     assertEquals(Azteca.getAttack, 70)
     assertEquals(Telstar.getAttack, 50)
 
@@ -71,31 +73,39 @@ class AWeaponTest extends FunSuite{
 
   /**
    * Test case for `getOwner` method.
-   * This test verifies if the returned owner character for an axe (`Tango`) matches the expected character (`Casemiro`).
+   * This test verifies if the returned owner character for sword 'Tango', axe `Jabulani`, bow 'Cachanna',
+   * * staff 'Azteca' and wand 'Telstar' matches the expected values (None at the beginning)
    */
   test("getOwner") {
     assertEquals(Tango.getOwner, None)
     assertEquals(Jabulani.getOwner, None)
-    assertEquals(Cachaña.getOwner, None)
+    assertEquals(Cachanna.getOwner, None)
     assertEquals(Azteca.getOwner, None)
     assertEquals(Telstar.getOwner, None)
-
   }
 
   /**
    * Test case for 'setOwner' method.
-   * This test verifies if the returned owner character for an axe ('Tango') matches with the playable who was previous set ('Casemiro')
+   * This test verifies if the returned owner character for sword 'Tango', axe `Jabulani`, bow 'Cachanna',
+   * * staff 'Azteca' and wand 'Telstar' matches the expected values (Pele, Forlan, Zidane, Maradona and Mbappe)
    */
   test("setOwner") {
     var Pele: Playable = new Paladin("Pele")
-    var Maradona : Playable = new Ninja("Maradona")
-    var Rossi: Playable = new Warrior("Rossi")
-    var Zidane: Playable = new WhiteMagican("Zidane")
+    var Forlan: Playable = new Warrior("Forlan")
+    var Zidane: Playable = new Ninja("Zidane")
+    var Mbappe: Playable = new WhiteMagican("Mbappe")
+    var Maradona : Playable = new BlackMagican("Maradona")
 
+    Pele.putWeapon(Tango)        // Paladin + sword
+    Forlan.putWeapon(Jabulani)   // Warrior + Axe
+    Zidane.putWeapon(Cachanna)    // Ninja + Bow
+    Maradona.putWeapon(Azteca)   // BlackMagican + Staff
+    Mbappe.putWeapon(Telstar)    // WhiteMagican + Wand
 
+    assertEquals(Tango.getOwner, Some(Pele))
+    assertEquals(Jabulani.getOwner, Some(Forlan))
+    assertEquals(Cachanna.getOwner, Some(Zidane))
+    assertEquals(Azteca.getOwner, Some(Maradona))
+    assertEquals(Telstar.getOwner, Some(Mbappe))
   }
-
-
-
-
 }
--- src/test/scala/weapons/common/AxeTest.scala
+++ src/test/scala/weapons/common/AxeTest.scala
@@ -1,6 +1,6 @@
 package weapons.common
 
-import model.playable.Playable
+
 import model.weapons.common.{Axe, Sword}
 import munit.FunSuite
 
@@ -9,11 +9,10 @@ import munit.FunSuite
  * This class contains tests for various functionalities of the `Axe` class.
  */
 class AxeTest extends FunSuite {
-  var Hacha: Axe = _ // Declaration of a variable `Hacha` of type `Axe`.
-  var Casemiro: Playable = _ // Declaration of a variable `Casemiro` of type `Playable`.
-
+  var Hacha: Axe = _
   var Hacha2: Axe = _
   var Espada: Sword = _
+
   /**
    * Set up the test environment before each test case.
    * This method is invoked before each test case (`test`) to initialize the test objects.
@@ -21,8 +20,7 @@ class AxeTest extends FunSuite {
    * @param context The context provided by the test framework (`BeforeEach`).
    */
   override def beforeEach(context: BeforeEach): Unit = {
-    // Creates a new instance of the `Axe` class with the specified parameters:
-    Hacha = new Axe() // - Name: Hacha // - Weight: 25 // - Attack: 50 // - Owner: Modric
+    Hacha = new Axe()
     Hacha2 = new Axe("Hacha2", 50,50)
     Espada = new Sword()
   }
@@ -35,64 +33,6 @@ class AxeTest extends FunSuite {
     assertEquals(Hacha.equals(Hacha),true)
     assertEquals(Hacha.equals(Hacha2),false)
     assertEquals(Hacha.equals(Espada),false)
-  }
-
-  /**
-   * Test case for `getName` method.
-   * This test verifies if the returned name value for an axe (`Hacha`) matches the expected value.
-   */
-  test("getName") {
-    // Retrieve the name value of the axe `Hacha` and store it in the `name2` variable.
-    var name2 = Hacha.getName
-    // Compare the actual name value (`name2`) with the expected value ("Hacha").
-    assertEquals(name2, "Hacha")
-  }
-
-  /**
-   * Test case for `getWeight` method.
-   * This test verifies if the returned weight value for an axe (`Hacha`) matches the expected value (`25`).
-   */
-  test("getWeight") {
-    // Retrieve the weight value of the axe `Hacha` and store it in the `weight` variable.
-    var weight = Hacha.getWeight
-    // Define the expected weight value.
-    var expected: Int = 50
-    // Compare the actual weight value (`weight`) with the expected value (`expected`).
-    assertEquals(weight, expected)
-  }
-
-  /**
-   * Test case for `getAttack` method.
-   * This test verifies if the returned attack value for an axe (`Hacha`) matches the expected value (`50`).
-   */
-  test("getAttack") {
-    // Retrieve the attack value of the axe `Hacha` and store it in the `attack` variable.
-    var attack = Hacha.getAttack
-    // Define the expected attack value.
-    var expected: Int = 50
-    // Compare the actual attack value (`attack`) with the expected value (`expected`).
-    assertEquals(attack, expected)
-  }
-
-  /**
-   * Test case for `getOwner` method.
-   * This test verifies if the returned owner character for an axe (`Hacha`) matches the expected character (`Casemiro`).
-   */
-  test("getOwner") {
-    // Retrieve the owner character of the axe `Hacha` and store it in the `owner` variable.
-    var owner: Any = Hacha.getOwner
-    // Define the expected owner character.
-    // Compare the actual owner character (`owner`) with the expected character (`expected`).
-    assertEquals(owner, None)
-  }
-
-  /**
-   * Test case for 'setOwner' method.
-   * This test verifies if the returned owner character for an axe ('Hacha') matches with the playable who was previous set ('Casemiro')
-   */
-  test("setOwner"){
-    Hacha.setOwner(Casemiro)
-    assertEquals(Hacha.getOwner,Some(Casemiro))
   }
 
 }
