1b8f67adc0a7d8b9403713e7743f645689c69d9b
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-06-02 16:32:23 -0400

    TEST: added documentation in class 'ASpellTest'

--- src/test/scala/spellTest/ASpellTest.scala
+++ src/test/scala/spellTest/ASpellTest.scala
@@ -5,15 +5,44 @@ import model.spell.dark.{Fire, Thunder}
 import model.spell.light.{Healing, Paralysis, Poison}
 import munit.FunSuite
 
-class ASpellTest extends FunSuite{
+/**
+ * A test suite for testing various spell functionalities.
+ * This class extends FunSuite, providing a framework for writing and running tests.
+ */
+class ASpellTest extends FunSuite {
+
+  /**
+   * A variable representing the Fire spell, which is a type of Spell.
+   * This variable is initialized in the beforeEach method.
+   */
   var Fire: Spell = _
+
+  /**
+   * A variable representing the Thunder spell, which is a type of Spell.
+   * This variable is initialized in the beforeEach method.
+   */
   var Thunder: Spell = _
+
+  /**
+   * A variable representing the Healing spell, which is a type of Spell.
+   * This variable is initialized in the beforeEach method.
+   */
   var Healing: Spell = _
+
+  /**
+   * A variable representing the Paralysis spell, which is a type of Spell.
+   * This variable is initialized in the beforeEach method.
+   */
   var Paralysis: Spell = _
+
+  /**
+   * A variable representing the Poison spell, which is a type of Spell.
+   * This variable is initialized in the beforeEach method.
+   */
   var Poison: Spell = _
 
   /**
-   * Set up the test environment before each test case.
+   * Sets up the test environment before each test case.
    * This method is invoked before each test case (`test`) to initialize the test objects.
    *
    * @param context The context provided by the test framework (`BeforeEach`).
@@ -26,12 +55,15 @@ class ASpellTest extends FunSuite{
     Poison = new Poison()
   }
 
-  test("getCost"){
-    assertEquals(Fire.getCost,15)
-    assertEquals(Thunder.getCost,20)
-    assertEquals(Healing.getCost,15)
-    assertEquals(Paralysis.getCost,25)
-    assertEquals(Poison.getCost,30)
+  /**
+   * Test case to check the cost of various spells.
+   * This test verifies that the cost of each spell is correctly returned by the getCost method.
+   */
+  test("getCost") {
+    assertEquals(Fire.getCost, 15)
+    assertEquals(Thunder.getCost, 20)
+    assertEquals(Healing.getCost, 15)
+    assertEquals(Paralysis.getCost, 25)
+    assertEquals(Poison.getCost, 30)
   }
-
 }
