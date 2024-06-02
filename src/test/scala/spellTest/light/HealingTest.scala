24791916bd84c2153ac5bca83d98b11cbf6fa601
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-06-02 16:34:30 -0400

    TEST: added documentation in class 'HealingTest'

--- src/test/scala/spellTest/light/HealingTest.scala
+++ src/test/scala/spellTest/light/HealingTest.scala
@@ -6,12 +6,26 @@ import model.spell.Spell
 import model.spell.light.Healing
 import munit.FunSuite
 
-class HealingTest extends FunSuite{
+/**
+ * A test suite for testing the functionality of the Healing spell.
+ * This class extends FunSuite, providing a framework for writing and running tests.
+ */
+class HealingTest extends FunSuite {
+
+  /**
+   * A variable representing the Healing spell, which is a type of Spell.
+   * This variable is initialized in the beforeEach method.
+   */
   var Healing: Spell = _
+
+  /**
+   * A variable representing a White Magician named Guler.
+   * This variable is initialized in the beforeEach method.
+   */
   var Guler: WhiteMagican = _
 
   /**
-   * Set up the test environment before each test case.
+   * Sets up the test environment before each test case.
    * This method is invoked before each test case (`test`) to initialize the test objects.
    *
    * @param context The context provided by the test framework (`BeforeEach`).
@@ -21,8 +35,12 @@ class HealingTest extends FunSuite{
     Guler = new WhiteMagican("Arda")
   }
 
-  test("canSelectedBy"){
-    assertEquals(Healing.canBeSelectedBy(Guler),true) // resto de casos seran probados en flujo de select spell en clases de pj magicos
+  /**
+   * Test case to check if the Healing spell can be selected by the White Magician Guler.
+   * This test verifies that the Healing spell can be selected by Guler.
+   */
+  test("canSelectedBy") {
+    assertEquals(Healing.canBeSelectedBy(Guler), true)
   }
 
 }
