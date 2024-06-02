2ae797066c891c449d91edd2d9dd6d2661fbc1df
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-06-02 16:49:55 -0400

    FEAT: added documentation in class 'Thunder'

--- src/main/scala/model/spell/dark/Fire.scala
+++ src/main/scala/model/spell/dark/Fire.scala
@@ -1,10 +1,21 @@
 package model.spell.dark
 
-import exceptions.spells.InvalidspellTarget
-import model.general.GameUnit
 import model.playable.magic.AMagicPlayable
 import model.spell.ASpell
+
+/**
+ * Represents a Fire spell, a type of dark spell.
+ * Inherits properties and behaviors from ASpell.
+ */
 class Fire extends ASpell {
+  /** The cost of casting the Fire spell. */
   val cost: Int = 15
+
+  /**
+   * Determines if the Fire spell can be selected by a player.
+   *
+   * @param player The player attempting to select the Fire spell.
+   * @return True if the Fire spell can be selected by the player, false otherwise.
+   */
   def canBeSelectedBy(player: AMagicPlayable): Boolean = player.canSelectFire()
 }
