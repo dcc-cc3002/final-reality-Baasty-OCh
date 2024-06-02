62bdd41bb51ac91eb87231a7e4725010b2b27d8c
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-06-02 16:35:24 -0400

    TEST: added documentation in class 'FireTest'

--- src/test/scala/spellTest/dark/FireTest.scala
+++ src/test/scala/spellTest/dark/FireTest.scala
@@ -5,12 +5,26 @@ import model.spell.Spell
 import model.spell.dark.Fire
 import munit.FunSuite
 
-class FireTest extends FunSuite{
+/**
+ * A test suite for testing the functionality of the Fire spell.
+ * This class extends FunSuite, providing a framework for writing and running tests.
+ */
+class FireTest extends FunSuite {
+
+  /**
+   * A variable representing the Fire spell, which is a type of Spell.
+   * This variable is initialized in the beforeEach method.
+   */
   var Fire: Spell = _
+
+  /**
+   * A variable representing a Black Magician named Brahim.
+   * This variable is initialized in the beforeEach method.
+   */
   var Brahim: BlackMagican = _
 
   /**
-   * Set up the test environment before each test case.
+   * Sets up the test environment before each test case.
    * This method is invoked before each test case (`test`) to initialize the test objects.
    *
    * @param context The context provided by the test framework (`BeforeEach`).
@@ -20,9 +34,12 @@ class FireTest extends FunSuite{
     Brahim = new BlackMagican("Brahim")
   }
 
-  test("canSelectedBy"){
-    assertEquals(Fire.canBeSelectedBy(Brahim),true) // resto de casos seran probados en flujo de select spell en clases de pj magicos
+  /**
+   * Test case to check if the Fire spell can be selected by the Black Magician Brahim.
+   * This test verifies that the Fire spell can be selected by Brahim.
+   */
+  test("canSelectedBy") {
+    assertEquals(Fire.canBeSelectedBy(Brahim), true)
   }
-
 
 }
