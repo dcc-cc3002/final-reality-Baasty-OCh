ed5e5bce7666eaa699c1deef5de1c39938072b30
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-06-02 16:46:14 -0400

    FEAT: added documentation in class 'Healing''

--- src/main/scala/model/spell/light/ALightSpell.scala
+++ src/main/scala/model/spell/light/ALightSpell.scala
@@ -2,7 +2,10 @@ package model.spell.light
 
 import model.spell.ASpell
 
+/**
+ * Abstract class representing a light spell in the game.
+ * This class extends ASpell and inherits its properties and behaviors.
+ */
 abstract class ALightSpell extends ASpell {
-
 
 }
--- src/main/scala/model/spell/light/Healing.scala
+++ src/main/scala/model/spell/light/Healing.scala
@@ -1,14 +1,36 @@
 package model.spell.light
 
 import exceptions.spells.InvalidspellTarget
-import model.general.GameUnit
 import model.playable.magic.AMagicPlayable
 
+/**
+ * Represents a Healing spell, a type of light spell.
+ * Inherits properties and behaviors from ALightSpell.
+ */
 class Healing extends ALightSpell {
-  val cost: Int  = 15
+  /** The cost of casting the Healing spell. */
+  val cost: Int = 15
+
+  /**
+   * Determines if the Healing spell can act on playable units.
+   *
+   * @return Always returns true, as the Healing spell can act on playable units.
+   */
   override def actOnPlayable(): Boolean = true
+
+  /**
+   * Throws an InvalidspellTarget exception as the Healing spell cannot act on enemy units.
+   *
+   * @throws InvalidspellTarget Always thrown to indicate that the Healing spell cannot act on enemy units.
+   */
   override def actOnEnemy(): Boolean = throw new InvalidspellTarget
 
+  /**
+   * Determines if the Healing spell can be selected by a player.
+   *
+   * @param player The player attempting to select the Healing spell.
+   * @return True if the Healing spell can be selected by the player, false otherwise.
+   */
   override def canBeSelectedBy(player: AMagicPlayable): Boolean = player.canSelectHealing()
-
 }
+
