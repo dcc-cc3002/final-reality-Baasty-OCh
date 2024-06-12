4d8dbea2760c484eadaa3c1fa94c814bef9296c8
Baasty-Och <bastian.olguin@ug.uchile.cl>
2024-06-12 15:14:50 -0400

    FEAT: added documentation to object 'Main'

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,14 @@
 import model.general.BattleGame
 
+/**The main object to run the battle game. */
 object Main {
+
+  /**
+   * The main method to start the battle game.
+   * @param args The command-line arguments (not used in this application). */
   def main(args: Array[String]): Unit = {
     val game = new BattleGame()
     game.run()
   }
-}
 
+}
